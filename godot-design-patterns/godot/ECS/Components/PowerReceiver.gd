36d7affb0eddde1c5f7af85875291ff5e3a41ce8
Douglas Webster <douglas.webster2107@gmail.com>
2021-03-27 12:52:54 +0000

    Removed files from index (now ignored)

--- godot-design-patterns/godot/ECS/Components/PowerReceiver.gd
+++
@@ -1,9 +0,0 @@
-class_name PowerReceiver
-extends Node
-
-#warning-ignore: unused_signal
-signal power_received(power, delta)
-
-export var power_required := 50.0
-
-var efficiency := 0.0
--- godot-design-patterns/godot/ECS/Components/PowerSource.gd
+++
@@ -1,9 +0,0 @@
-class_name PowerSource
-extends Node
-
-#warning-ignore: unused_signal
-signal power_drawn(power, delta)
-
-export var power_amount := 50.0
-
-var efficiency := 0.0
