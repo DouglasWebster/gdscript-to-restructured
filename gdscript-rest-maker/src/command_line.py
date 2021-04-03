bde68ef7193f02d97d538787ffde9d5b473fafdb
Douglas Webster <douglas.webster2107@gmail.com>
2021-04-03 17:04:20 +0100

    Corrected spelling mistakes.

--- gdscript-rest-maker/src/command_line.py
+++ gdscript-rest-maker/src/command_line.py
@@ -23,7 +23,7 @@ def parse(args=sys.argv) -> Namespace:
     parser.add_argument("-p", "--path", type=str, default="export", help="Path to the output directory.")
     parser.add_argument("-i", "--make-index", action="store_true", default=False, help="If this flag is present, create and index.rst page with a table of contents.")
     parser.add_argument("-v", "--verbose", action="count", default=0, help="Set the verbosity level. For example -vv sets verbosity to level 2. Defalt: 0.")
-    parser.add_argument("--dry-run", action="store_true", help="Run the script at max verbosity without creating files.  For debuggin puposese.")
+    parser.add_argument("--dry-run", action="store_true", help="Run the script at max verbosity without creating files.  For debugging purposes.")
     parser.add_argument("-V", "--version", action="store_true", help="Print the version number and exit,")
     parser.add_argument("--doc-version", type=str, default="0.0.0", help="Set the document version number if there is no version set in the JSON file. Defaults to 0.0.0")
 
