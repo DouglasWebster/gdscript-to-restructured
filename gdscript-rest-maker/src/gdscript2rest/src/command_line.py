f3445d65607510fcf796bd27ffc3f7c6a9113b6b
Douglas Webster <douglas.webster2107@gmail.com>
2022-09-13 11:29:10 +0100

    Updated parsing of command line

--- gdscript-rest-maker/src/gdscript2rest/src/command_line.py
+++ gdscript-rest-maker/src/gdscript2rest/src/command_line.py
@@ -2,14 +2,6 @@
 import sys
 from argparse import ArgumentParser, Namespace
 
-# def _set_date(args) -> datetime.date:
-#     """Validates the date argument, parsing the date from the ISO format"""
-#     date: datetime.date
-#     try:
-#         date = datetime.date.fromisoformat(args)
-#     except ValueError:
-#         date = datetime.date.today()
-#     return date
 
 def parse(args=sys.argv) -> Namespace:
     parser: ArgumentParser = ArgumentParser(
@@ -19,10 +11,12 @@ def parse(args=sys.argv) -> Namespace:
         "use with Sphinx.",
     )
 
+    args.pop(0)  # we dont want the file list polluted with the program name.
+
     parser.add_argument("files", type=str, nargs="+", help="A list of paths to JSON files.")
     parser.add_argument("-p", "--path", type=str, default="export", help="Path to the output directory.")
     parser.add_argument("-i", "--make-index", action="store_true", default=False, help="If this flag is present, create and index.rst page with a table of contents.")
-    parser.add_argument("-v", "--verbose", action="count", default=0, help="Set the verbosity level. For example -vv sets verbosity to level 2. Defalt: 0.")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Set the verbosity level. For example -vv sets verbosity to level 2. Default: 0.")
     parser.add_argument("--dry-run", action="store_true", help="Run the script at max verbosity without creating files.  For debugging purposes.")
     parser.add_argument("-V", "--version", action="store_true", help="Print the version number and exit,")
     parser.add_argument("--doc-version", type=str, default="0.0.0", help="Set the document version number if there is no version set in the JSON file. Defaults to 0.0.0")
